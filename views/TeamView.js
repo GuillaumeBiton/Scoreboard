3e7d625d4545ed1bb67069df750d55567ba9df78
Guillaume Biton <guillaume.biton@free.fr>
2011-11-15 16:26:11 +0100

    Add change name Feature

--- views/TeamView.js
+++ views/TeamView.js
@@ -4,7 +4,8 @@ var TeamView = Backbone.View.extend({
 	template: _.template("<div id='name' class='name'><%= name %></div><div id='score 'class='score'><%= point %></div>"),
 	
 	events: {
-		"click .score": "score"
+		"click .score": "score",
+		"dblclick .name": "changeName"
 	},
 	
 	initialize: function(){
@@ -19,5 +20,10 @@ var TeamView = Backbone.View.extend({
 	
 	score: function(){
 		this.model.set({ point: this.model.get('point')+1});
+	},
+	
+	changeName: function(){
+		var name = prompt('Change team name :', this.model.get('name'));
+		this.model.set({name: name});
 	}
 });
